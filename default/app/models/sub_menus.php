f56f9924f8d0e2742aea694274f92e71a125b8ab
FiDeLio <fidel.oyarzo@gmail.com>
2010-12-09 20:14:49 -0300

    correcion pequeña

--- default/app/models/sub_menus.php
+++ default/app/models/sub_menus.php
@@ -2,8 +2,6 @@
 /**
  * @author Fidel Oyarzo <fidel.oyarzo@gmail.com>
  */
-?>
-<?php 
 class SubMenus extends ActiveRecord{
     protected function initialize(){
         $this->belongs_to('controllers');
